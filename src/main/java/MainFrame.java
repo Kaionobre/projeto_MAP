055797b5eab757541148eec12d434b22c4b192f1
Iago Dutra Garcia <iagodutra270@gmail.com>
2024-12-06 13:04:32 -0300

    Escolha de Cargo na Interface

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainFrame extends JFrame {
-    // Lista global para armazenar as tasks
     private List<Task> taskList;
+    private Time time; // Usando Time, que pode ser PO, Scrum ou Analista
 
     public MainFrame() {
         setTitle("Gerenciador de Projetos");
@@ -17,8 +17,22 @@ public class MainFrame extends JFrame {
         this.taskList = new ArrayList<>();
         this.taskList.add(new Task("Tarefa Inicial")); // Adicionando uma tarefa inicial
 
-        // Passa a lista de tarefas para o TaskManagementScreen
-        showScreen(new TaskManagementScreen(this, taskList));
+        // Solicita o nome, senha e cargo do usuário
+        String nome = JOptionPane.showInputDialog("Digite seu nome:");
+        String senha = JOptionPane.showInputDialog("Digite sua senha:");
+        String cargo = (String) JOptionPane.showInputDialog(null, "Escolha seu cargo:", "Seleção de Cargo", JOptionPane.QUESTION_MESSAGE, null, new String[]{"PO (Product Owner)", "Scrum", "Analista"}, "PO (Product Owner)");
+
+        // Cria o objeto Time com base no cargo selecionado
+        if (cargo.equals("PO (Product Owner)")) {
+            time = new PO(nome, senha);
+        } else if (cargo.equals("Scrum")) {
+            time = new ScrumMaster(nome, senha);
+        } else {
+            time = new Analista(nome, senha);
+        }
+
+        // Passa a lista de tarefas e o time para o TaskManagementScreen
+        showScreen(new TaskManagementScreen(this, taskList, time)); // Passando os três parâmetros
     }
 
     public void showScreen(JPanel panel) {
--- src/main/java/ScrumMaster.java
+++ src/main/java/ScrumMaster.java
@@ -1,5 +1,10 @@
 public class ScrumMaster extends Time {
     public ScrumMaster(String nome, String senha) {
-        super(nome, senha, "Scrum Master");
+        super(nome, senha, "Scrum");
+    }
+
+    @Override
+    public boolean podeCriarTarefas() {
+        return true; // Scrum também pode criar tarefas
     }
 }
